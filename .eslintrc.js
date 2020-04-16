160d4bcd75b04863fba33eb48f426f0d95d002d2
Lucas Teles <lucas.teles@lambda3.com.br>
2020-04-16 03:12:20 -0300

    WIP

---
+++ .eslintrc.js
@@ -0,0 +1,45 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "node": true
+    },
+    "parser": "@typescript-eslint/parser",
+    "extends": [
+        "plugin:react/recommended", // Uses the recommended rules from @eslint-plugin-react
+        "plugin:@typescript-eslint/recommended" // Uses the recommended rules from @typescript-eslint/eslint-plugin
+    ],
+    "parserOptions": {
+        "project": "tsconfig.json",
+        "sourceType": "module",
+        "parserOptions": {
+            "ecmaFeatures": {
+                jsx: true, 
+                tsx: true 
+            }
+        },
+    },
+    "plugins": [
+        "@typescript-eslint", "eslint-plugin-react"
+    ],
+    "rules": {
+        "@typescript-eslint/member-delimiter-style": [
+            "error",
+            {
+                "multiline": {
+                    "delimiter": "none",
+                    "requireLast": true
+                },
+                "singleline": {
+                    "delimiter": "semi",
+                    "requireLast": false
+                }
+            }
+        ],
+        "@typescript-eslint/semi": [
+            "error",
+            "never"
+        ],
+        "@typescript-eslint/explicit-function-return-type": "off"
+
+    }
+}
